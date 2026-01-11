95c4aec27e7fae3040e5848f6123da1a248e50d7
nitin798563 <ansingh6388@gmail.com>
2026-01-11 12:34:40 +0530

    fix error

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -9,7 +9,7 @@ const syncUserCreation = inngest.createFunction(
     { id: "sync-user-from-clerk" },
     { event: "clerk/user.created" },
     async ({ event }) => {
-        const { id, first_name, last_name, email_address, image_url } = event.data;
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
         let username = email_addresses[0].email_address.split("@")[0];
 
         //check availability of username in the database
