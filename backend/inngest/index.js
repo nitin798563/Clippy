ba7e1644d16eb7c82b83f006a5bedbea9045b268
nitin798563 <ansingh6388@gmail.com>
2026-01-24 22:36:06 +0530

    Create API

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "clippy-app" });
@@ -24,7 +25,7 @@ const syncUserCreation = inngest.createFunction(
             profile_picture: image_url,
             username,
         }
-        await new User(userData).save();
+        await User(userData).save();
     })
 
 //Inggest function to update user data in the database
@@ -32,28 +33,114 @@ const syncUserUpdation = inngest.createFunction(
     { id: "update-user-from-clerk" },
     { event: "clerk/user.updated" },
     async ({ event }) => {
-        const { id, first_name, last_name, email_address, image_url } = event.data;
-        
-    const updatedUSerData = {
-        email: email_addresses[0].email_address,
-        full_name: first_name + " " + last_name,
-        profile_picture: image_url,
-    }
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+        const updatedUSerData = {
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url,
+        }
         await User.findByIdAndUpdate(id, updatedUSerData)
     })
 
-    //Innggest function to delete user data from the database
-    const syncUserDeletion = inngest.createFunction(
+//Innggest function to delete user data from the database
+const syncUserDeletion = inngest.createFunction(
     { id: "delete-user-from-clerk" },
     { event: "clerk/user.deleted" },
     async ({ event }) => {
-        const { id} = event.data;
+        const { id } = event.data;
         await User.findByIdAndDelete(id)
     })
+
+//Inngest function to send Reminder when a new connection request is added
+const sendNewConnectionReuestReminder = inngest.createFunction(
+    { id: "send-new-connection-request-reminder" },
+    { event: "app/connection-request" },
+    async ({ event, step }) => {
+        const { connectionId } = event.data;
+
+        await step.run('send-connection-request-mail', async () => {
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+            const subject = `👋 New Connection Request`;
+            const body = `<div style="font-family: Arial, sans-serif; padding: 20px;">
+                          <h2>Hi ${connection.to_user_id.full_name},</h2>
+                        <p>
+                          You have a new connection request from <strong>${connection.from_user_id.full_name}</strong> 
+                       - @${connection.from_user_id.username}
+                         </p>
+                         <p>
+                         Click 
+                         <a 
+                         href="${process.env.FRONTEND_URL}/connections"
+                         style="color: #10b981; text-decoration: none; font-weight: bold;"
+                          >
+                         here
+                         </a>
+                         to accept or reject the request
+                         </p>
+
+                        <br />
+
+                         <p>
+                          Thanks,<br />
+                         Clippy – Stay Connected
+                         </p>
+                           </div>`;
+
+            await sendEmail({
+                to: connection.to_user_id.email,
+                subject,
+                body
+            })
+        })
+        const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000)
+        await step.sleepUntill('wait-for-24-hours', in24Hours);
+        await step.run('send-connection-request-reminder', async () => {
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+
+            if (connection.status === 'accepted') {
+                return { message: 'Already accepted' }
+            }
+            const subject = `👋 New Connection Request`;
+            const body = `<div style="font-family: Arial, sans-serif; padding: 20px;">
+                          <h2>Hi ${connection.to_user_id.full_name},</h2>
+                        <p>
+                          You have a new connection request from <strong>${connection.from_user_id.full_name}</strong> 
+                       - @${connection.from_user_id.username}
+                         </p>
+                         <p>
+                         Click 
+                         <a 
+                         href="${process.env.FRONTEND_URL}/connections"
+                         style="color: #10b981; text-decoration: none; font-weight: bold;"
+                          >
+                         here
+                         </a>
+                         to accept or reject the request
+                         </p>
+
+                        <br />
+
+                         <p>
+                          Thanks,<br />
+                         Clippy – Stay Connected
+                         </p>
+                           </div>`;
+
+            await sendEmail({
+                to: connection.to_user_id.email,
+                subject,
+                body
+            })
+            return{message: "Reminder sent."}
+        })
+    }
+)
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    sendNewConnectionReuestReminder
 ];
