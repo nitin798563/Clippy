91e530d31f63c6a98ed1a7bbd92f36da9a3b4c86
nitin798563 <ansingh6388@gmail.com>
2026-01-26 21:20:28 +0530

    change file name

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -11,6 +11,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
   const navigate = useNavigate()
   const user = useSelector((state) => state.user.value)
   const { signOut } = useClerk()
+  
 
   return (
     <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 $ 
