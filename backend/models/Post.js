24eedaf6eb1ca36906bf922db3a27bd2ee3a52a6
nitin798563 <ansingh6388@gmail.com>
2026-02-01 19:29:05 +0530

    Create api for delete post and add comment

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -5,7 +5,19 @@ const postSchema = new mongoose.Schema({
     content: { type: String },
     image_urls: [{ type: String }],
     post_type: { type: String, enum: ['text', 'image', 'text_with_image'], required: true },
-    likes_count: [{ type: String, ref: 'User' }]
+    likes_count: [{ type: String, ref: 'User' }],
+    share_count: { type: Number, default: 0 },
+    comments: [
+        {
+            user: {
+                _id: String,
+                full_name: String,
+                profile_picture: String
+            },
+            text: String,
+            createdAt: { type: Date, default: Date.now }
+        }
+    ],
 }, { timestamps: true, minimize: false })
 
 const Post = mongoose.model('Post', postSchema)
