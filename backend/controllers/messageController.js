71645d192370dac8aa5deef46625293d9ab853fb
nitin798563 <ansingh6388@gmail.com>
2026-04-27 14:54:11 +0530

    add socketio for realtime lice chat

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -2,78 +2,122 @@ import fs from 'fs'
 import imagekit from '../configs/imagekit.js';
 import Message from '../models/Message.js';
 //Create an empty object to store SS Event connections
-const connections = {};
+//const connections = {};
 
 //Controller function for the SSE endpoint
-export const sseController = async (req, res) => {
-    const { userId } = req.params
-    console.log('New Client Connected:', userId)
+// export const sseController = async (req, res) => {
+//     const { userId } = req.params
+//     console.log('New Client Connected:', userId)
 
-    //Set SSE headers
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     //Set SSE headers
+//     res.setHeader('Content-Type', 'text/event-stream');
+//     res.setHeader('Cache-Control', 'no-cache');
+//     res.setHeader('Connection', 'keep-alive');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    //Add the client's response object to the connnections object
-    connections[userId] = res
+//     //Add the client's response object to the connnections object
+//     connections[userId] = res
 
-    //Send an initial event to the client
-    res.write('log: Connected to SSE stream\n\n');
+//     //Send an initial event to the client
+//     res.write('log: Connected to SSE stream\n\n');
 
-    //Handle client disconnection
-    req.on('close', () => {
-        //Remove the client's response object from the connections array
-        delete connections[userId];
-        console.log('Client disconnected')
-    })
-}
+//     //Handle client disconnection
+//     req.on('close', () => {
+//         //Remove the client's response object from the connections array
+//         delete connections[userId];
+//         console.log('Client disconnected')
+//     })
+// }
 
 //Send Message
+// export const sendMessage = async (req, res) => {
+//     try {
+//         const { userId } = req.auth();
+//         const { to_user_id, text } = req.body;
+//         const image = req.file;
+
+//         let media_url = '';
+//         let message_type = image ? 'image' : 'text';
+//         if (message_type === 'image') {
+//             const fileBuffer = fs.readFileSync(image.path)
+//             const response = await imagekit.files.upload({
+//                 file: fileBuffer.toString("base64"),
+//                 fileName: image.originalname,
+//                  folder: "/messages",
+//             })
+//             media_url = `${process.env.IMAGEKIT_URL_ENDPOINT}/${response.filePath}?tr=w-1280,f-webp,q-auto`;
+//             // delete temp file
+//             fs.unlinkSync(image.path);
+
+//         }
+//         const message = await Message.create({
+//             from_user_id: userId,
+//             to_user_id,
+//             text,
+//             message_type,
+//             media_url
+//         })
+//         res.json({ success: true, message });
+
+//         //Send massage to to_user_id using SSE
+
+//         const messageWithUserData = await Message.findById(message._id).populate('from_user_id')
+
+//         if (connections[to_user_id]) {
+//             connections[to_user_id].write(`data: ${JSON.stringify
+//                 (messageWithUserData)}\n\n`)
+//         }
+
+
+//     } catch (error) {
+//         console.log(error);
+//         res.json({ success: false, message: error.message });
+//     }
+// }
 export const sendMessage = async (req, res) => {
-    try {
-        const { userId } = req.auth();
-        const { to_user_id, text } = req.body;
-        const image = req.file;
+  try {
+    const { userId } = req.auth();
+    const { to_user_id, text } = req.body;
+    const image = req.file;
 
-        let media_url = '';
-        let message_type = image ? 'image' : 'text';
-        if (message_type === 'image') {
-            const fileBuffer = fs.readFileSync(image.path)
-            const response = await imagekit.files.upload({
-                file: fileBuffer.toString("base64"),
-                fileName: image.originalname,
-                 folder: "/messages",
-            })
-            media_url = `${process.env.IMAGEKIT_URL_ENDPOINT}/${response.filePath}?tr=w-1280,f-webp,q-auto`;
-            // delete temp file
-            fs.unlinkSync(image.path);
+    let media_url = '';
+    let message_type = image ? 'image' : 'text';
 
-        }
-        const message = await Message.create({
-            from_user_id: userId,
-            to_user_id,
-            text,
-            message_type,
-            media_url
-        })
-        res.json({ success: true, message });
+    if (message_type === 'image') {
+      const fileBuffer = fs.readFileSync(image.path);
+      const response = await imagekit.files.upload({
+        file: fileBuffer.toString("base64"),
+        fileName: image.originalname,
+        folder: "/messages",
+      });
 
-        //Send massage to to_user_id using SSE
-
-        const messageWithUserData = await Message.findById(message._id).populate('from_user_id')
-
-        if (connections[to_user_id]) {
-            connections[to_user_id].write(`data: ${JSON.stringify
-                (messageWithUserData)}\n\n`)
-        }
-
-
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
+      media_url = `${process.env.IMAGEKIT_URL_ENDPOINT}/${response.filePath}?tr=w-1280,f-webp,q-auto`;
+      fs.unlinkSync(image.path);
     }
-}
+
+    const message = await Message.create({
+      from_user_id: userId,
+      to_user_id,
+      text,
+      message_type,
+      media_url
+    });
+
+    const fullMessage = await Message.findById(message._id)
+      .populate("from_user_id");
+
+    // REAL TIME SEND
+    const io = req.app.get("io");
+
+    io.to(to_user_id).emit("receive_message", fullMessage);
+
+    res.json({ success: true, message });
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Get Chat Messages
 export const getChatMessages = async (req, res) => {
@@ -95,6 +139,7 @@ export const getChatMessages = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
 
 export const getUserRecentMessages = async (req, res) => {
     try {
